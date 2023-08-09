69286d017e67bd0765678912b0e6af3418cc0bfb
pablo4lv <93752198+pablo4lv@users.noreply.github.com>
2023-08-09 19:38:25 -0300

    a

--- js/validacion.js
+++ js/validacion.js
@@ -1,53 +1,41 @@
 function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
+    let alert = document.getElementById("alert-success")
+    alert.classList.add("show")
+    setTimeout(() => {alert.classList.remove("show")}, 3000);
 }
 
 function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
+    let alert = document.getElementById("alert-danger")
+    alert.classList.add("show");
+    setTimeout(() => {alert.classList.remove("show")}, 3000);
 }
+
+let password1 = document.getElementById("password1");
+let password2 = document.getElementById("password2");
+let fname = document.getElementById("nombre");
+let lname = document.getElementById("apellido");
+let email = document.getElementById("email");
+let btn = document.getElementById("regBtn");
+
+function psw(){
+    return ((password1.value === password2.value) && (password1.value.length >= 6))
+}
+
+function checked(){
+    return document.getElementById("terminos").checked;
+}
+
+function filled(){
+    return !(!fname.value && !lname.value && !email.value)
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-    const password1 = document.getElementById("password1");
-    const password2 = document.getElementById("password2");
-    const regBtn = document.getElementById("regBtn");
-    const alertSuccess = document.getElementById("alert-success");
-    const alertDanger = document.getElementById("alert-danger");
-
-regBtn.addEventListener("click", function () {
-    const nombre = document.getElementById("nombre").value;
-    const apellido = document.getElementById("apellido").value;
-    const email = document.getElementById("email").value;
-    const terminos = document.getElementById("terminos").checked;
-    const emailRegex = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
-
-    if (!nombre || !apellido || !email || !password1.value || !password2.value || !terminos) {
-      alertDanger.textContent = "Los datos ingresados no cumplen con los campos solicitados.";
-      alertDanger.classList.add("show");
-      setTimeout(() => {
-        alertDanger.classList.remove("show");
-      }, 3000);
-    } else if (password1.value.length < 6) {
-      alertDanger.textContent = "La contraseña debe tener mínimo 6 caracteres.";
-      alertDanger.classList.add("show");
-      setTimeout(() => {
-        alertDanger.classList.remove("show");
-      }, 3000);
-    } else if (password1.value !== password2.value) {
-      alertDanger.textContent = "Las contraseñas no son iguales.";
-      alertDanger.classList.add("show");
-      setTimeout(() => {
-        alertDanger.classList.remove("show");
-      }, 3000);
-    } else if (!emailRegex.test(email)) {
-        alertDanger.textContent = "Ingrese un correo electrónico válido.";
-        alertDanger.classList.add("show");
-        setTimeout(() => {
-          alertDanger.classList.remove("show");
-        }, 3000); 
-    } else {
-      alertSuccess.classList.add("show");
-      setTimeout(() => {
-        alertSuccess.classList.remove("show");
-      }, 3000);
-    }
+  
+    btn.addEventListener("click", function () {
+      if (psw() && checked() && filled()) {
+        showAlertSuccess()
+      } else {
+        showAlertError()
+      }
+    });
   });
-});
